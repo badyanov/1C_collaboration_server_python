fe6a61e54ebfb20e2b8e02c3e36995fd09190c37
badyanov <d.badyanov@outlook.com>
2022-06-20 22:26:03 +0300

    заготовка для отображения логов на главной странице

--- srv/cs/views.py
+++ srv/cs/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from .models import Group, Channel, Message, Log
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+class Index(ListView):
+    model = Log
+    paginate_by: int = 10
+    template_name: str = 'cs/index.html'
+
+class GroupList(ListView):
+    model = Group
+
+class GroupPost(DetailView):
+    model = Group
+
+class ChannelList(ListView):
+    model = Channel
+
+class ChannelPost(DetailView):
+    model = Channel
+
+class MessageList(ListView):
+    model = Message
+
+class MessagePost(DetailView):
+    model = Message
